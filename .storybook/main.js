c62c7f9c7f01c7ab4753deb5a33f4e215fcd3639
yushan <yushanwebdev@gmail.com>
2022-01-29 15:15:46 +0530

    feat: Remove the docs tab from the Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
+  ],
   framework: '@storybook/react',
   core: {
     builder: 'storybook-builder-vite',
