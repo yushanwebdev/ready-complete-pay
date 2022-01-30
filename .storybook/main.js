7703ef59ba49d50b58ce4b829ec1218edae6e712
yushan <yushanwebdev@gmail.com>
2022-01-30 12:33:30 +0530

    fix: Fix Storybook rendering issues after install MUI 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,5 +10,28 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: 'storybook-builder-vite',
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/react': path.resolve(
+            path.join(__dirname, '../node_modules/@emotion/react'),
+          ),
+          '@emotion/styled': path.resolve(
+            path.join(__dirname, '../node_modules/@emotion/styled'),
+          ),
+          '@emotion/core': path.resolve(
+            path.join(__dirname, '../node_modules/@emotion/react'),
+          ),
+          'emotion-theming': path.resolve(
+            path.join(__dirname, '../node_modules/@emotion/react'),
+          ),
+        },
+      },
+    };
   },
 };
